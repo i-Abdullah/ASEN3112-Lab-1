90ca135bf4b3fb09609a02b9c0a537f61186ccc1
Abdullah <iAbdullah.AlAmeri@gmail.com>
2019-09-21 21:23:55 -0600

    Added constants

--- Lab1.m
+++ Lab1.m
@@ -9,9 +9,31 @@ clear
 clc
 close all
 
-%% Constants:
+%% define onstants:
 
-G = 3.75 * 10 ^ 6 ; % psi;
+G = 3.75 * 10 ^ 6 ; % psi (shear modulus);
 
 
 % closed thin wall specimen:
+
+De_closed = 3/4 ; % in, exterior daiamter
+t_closed = 1/16 ; % in, thickness
+L_closed = 13 ; % inches, measured in lab.
+T_max_closed = 400; % (lbs-in), maximum torque
+Tau_max_closed = 8620; % psi, max shear stress
+FS_closed = mean([ 2.3 2.6]); % given in lab doc as range, so took the mean.
+
+
+
+% open thin wall specimen
+% everything same as closed, we assume cut is negligible :)
+
+De_open = 3/4 ; % in, exterior daiamter
+t_open = 1/16 ; % in, thickness
+L_open = 13 ; % inches, measured in lab.
+T_max_open = 20; % lbs -in
+Tau_max_open = 7800; % psi
+FS_open = 2.8; % factor of safety.
+
+%%
+
