6d60263aa36210c4268dd5ba35bf55fcb716d237
Abdullah <iAbdullah.AlAmeri@gmail.com>
2019-09-21 21:17:05 -0600

    Added data + started code

---
+++ Lab1.m
@@ -0,0 +1,17 @@
+%% Info:
+
+% this code is post-data collection analysis for two tubes, one is open and
+% one is closed, for more info, go to /Info
+
+%% housekeeping
+
+clear
+clc
+close all
+
+%% Constants:
+
+G = 3.75 * 10 ^ 6 ; % psi;
+
+
+% closed thin wall specimen:
